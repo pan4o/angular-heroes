ff0715a8ffcb0dd7d98a136f73bdf9d1a9f4aee2
pan4o <panchenko923@gmail.com>
2018-03-05 01:55:43 +0200

    add hell

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,17 +9,16 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
